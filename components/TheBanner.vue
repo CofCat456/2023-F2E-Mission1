c8ca2090355299cf3c375891714d2ea02160f0bd
cofcat <cofcat@cofcat-macpro.local>
2023-12-03 08:25:07 +0800

    feat(desktop): add animation to Banner

--- components/TheBanner.vue
+++ components/TheBanner.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
-import { inject } from 'vue'
-
 const headerHeight = inject('headerHeight')
+
+const { $gsap } = useNuxtApp()
+
+const titleRef = ref<HTMLHeadingElement | null>(null)
+const leftBtnRef = ref<HTMLDivElement | null>(null)
+const rightBtnRef = ref<HTMLDivElement | null>(null)
+const peopleRef = ref<HTMLImageElement | null>(null)
+
+onMounted(() => {
+  const timeline = $gsap.timeline()
+  timeline.from(titleRef.value, { y: 150, opacity: 0, duration: 0.8 })
+    .from([leftBtnRef.value, rightBtnRef.value], { y: 100, opacity: 0, duration: 0.8 })
+    .from(peopleRef.value, { y: 100, opacity: 0, duration: 0.8 }, '+=.1')
+})
 </script>
 
 <template>
@@ -13,18 +25,19 @@ const headerHeight = inject('headerHeight')
   >
     <div class="text-center inline-block mt-auto">
       <!-- Title -->
-      <h2 class="font-mantou text-8xl tracking-2 text-gradientTheme font-normal">
+      <h2 ref="titleRef" class="font-mantou text-8xl tracking-2 text-gradientTheme font-normal">
         台灣的明天 喵先鋪路
       </h2>
 
       <!-- Buttons -->
       <div class="font-inter my-4 flex items-center gap-4 justify-center">
-        <div class="bg-primary inline-block rounded-2xl py-3 px-4">
+        <div ref="leftBtnRef" class="bg-primary inline-block rounded-2xl py-3 px-4">
           <h3 class="text-white">
             2024 立委參選人
           </h3>
         </div>
-        <div class="flex items-center gap-4 border border-white shadow rounded-2xl px-4 py-3">
+
+        <div ref="rightBtnRef" class="flex items-center gap-5 border border-white shadow rounded-2xl px-4 py-3">
           <div class="relative w-10 bg-gradientTheme p-3 rounded-full aspect-square">
             <p class="text-white text-2xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
               3
@@ -38,7 +51,9 @@ const headerHeight = inject('headerHeight')
     </div>
 
     <!-- Image -->
-    <NuxtImg width="723" fit="cover" src="/people_1.png" class="mx-auto" />
+    <div ref="peopleRef" class="mx-auto">
+      <NuxtImg width="723" fit="cover" src="/people_1.png" />
+    </div>
   </section>
 </template>
 
